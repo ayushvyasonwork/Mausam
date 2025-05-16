bb0ecc671bbcfe9819a4c44efb3f9b2e328a6f53
Ayush Vyas <work.ayushvyas17@gmail.com>
2025-05-16 11:36:42 +0530

    update: switch API endpoint to production URL

--- Client/src/utils/api.jsx
+++ Client/src/utils/api.jsx
@@ -1,3 +1,3 @@
-//  const api='https://weather-app-4-9vwu.onrender.com'
-const api='http://localhost:5000'
+ const api='https://mausam-awjn.onrender.com'
+// const api='http://localhost:5000'
  export default api 
